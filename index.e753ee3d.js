eecc2c4d2a8033f967acc14de4568cecf186a860
Your_Name Your_Surname <your@email>
2024-10-19 10:58:14 +0300

    make build

---
+++ index.e753ee3d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img ");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.src=r.href});
+//# sourceMappingURL=index.e753ee3d.js.map

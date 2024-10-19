34cf6d083b929b25a2d8e318dd750f57f3f0f408
Your_Name Your_Surname <your@email>
2024-10-19 10:50:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const ul = document.querySelector('.gallery__list');
+const div = document.querySelector('.gallery__large-img ');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  div.src = link.href;
+});

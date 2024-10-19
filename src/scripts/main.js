c14788b025a5e8958566d04b131c98f94facf236
Your_Name Your_Surname <your@email>
2024-10-19 10:55:03 +0300

    new solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const ul = document.querySelector('.gallery__list');
-const div = document.querySelector('.gallery__large-img ');
+const divElement = document.querySelector('.gallery__large-img ');
 
 ul.addEventListener('click', (e) => {
   e.preventDefault();
 
   const link = e.target.closest('.list-item__link');
 
-  div.src = link.href;
+  divElement.querySelector('img').src = link.href;
 });
